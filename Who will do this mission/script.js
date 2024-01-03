3f30a4b4ef602749ef1d0bd967ba99c4f7bf398f
Parisara1992 <p.assawapreechaanan@gmail.com>
2024-01-03 16:59:06 +0700

    add who will do this mission app

---
+++ Who will do this mission/script.js
@@ -0,0 +1,25 @@
+// script.js
+$(document).ready(function() {
+  $("#pick").click(function() {
+    var names = $("#names").val();
+    var nameArray = names.split(',');
+    var winner = nameArray[Math.floor(Math.random() * nameArray.length)];
+    
+    var mission = $("#mission").val();
+    
+    if (mission.trim() !== "") {
+      winner = "Congratulations! " + winner + " " + mission+" ^_^";
+    } else {
+      winner = "Congratulations! " + winner;
+    }
+
+    $("#world, #winner, #close").addClass("open");
+    $("#winner").text(winner);
+  });
+
+  $("#close").click(function() {
+    $("#world, #winner, #close").removeClass("open");
+  });
+
+  // ... (Confetti code remains unchanged)
+});
